863a0193bb6a147b106f8e4304b93ab236cfc982
AikkiiiyDev <annylouza24@gmail.com>
2026-09-17 21:41:51 -0300

    Create oi.js

---
+++ oi.js
@@ -0,0 +1,21 @@
+const IMAGENS = {
+  // 🌱 CENÁRIO
+  grama: "https://imgur.com/a/zXvea9Z",
+  grama2: "https://imgur.com/a/VnfpZ3g",
+
+  // 🌸 FLORES
+  flor1: "https://imgur.com/a/jXEivgD",
+  flor2: "https://imgur.com/a/5ViZBOQ",
+  flor3: "https://imgur.com/a/fBh7UKi",
+
+  // 🌳 ÁRVORES
+  arvore1: "https://imgur.com/a/mwwbJw4",
+  arvore2: "https://imgur.com/a/PWejMRA",
+
+  // 🐾 BICHOS
+  gato: "https://imgur.com/a/P5HoaV9",
+  cachorro: "https://imgur.com/a/80sd3fV",
+  raposa: "https://imgur.com/a/ZuNZMrU",
+  coelho: "https://imgur.com/a/3KJcTq7",
+  urso: "https://imgur.com/a/piQyAwj"
+};
